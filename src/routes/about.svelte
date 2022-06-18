b7437061f6ba5ccbd89d60f0a69bf6e322ce4fe8
LWY <668915+laxa88@users.noreply.github.com>
2022-06-18 16:15:06 +0900

    feat: add reusable Title component

---
+++ src/routes/about.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Title from '$lib/title.svelte';
+</script>
+
+<div class="about">
+	<Title title="About us" />
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos rerum veniam corrupti
+		labore. Magnam, rerum veritatis. Consectetur soluta tenetur cum dolore. Tempore accusantium
+		pariatur ipsum sed assumenda consequatur nostrum officiis?
+	</p>
+
+	<a href="/guides">View Guides</a>
+	<a href="/">Home</a>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Title from '$lib/title.svelte';
+
 	let title = 'Ninja Gaming Guides';
 
 	const setTitle = () => {
@@ -7,11 +9,20 @@
 </script>
 
 <div class="index">
-	<h2>{title}</h2>
-	<p>Lorem ipsum dolor sit amet</p>
+	<Title>{title}</Title>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eaque debitis excepturi impedit
+		et, perspiciatis optio dolorem similique exercitationem beatae voluptas, quisquam deserunt natus
+		nam repellat id quam at iure!
+	</p>
 
-	<button on:click={setTitle}>Change tile</button>
+	<button on:click={setTitle}>Change title</button>
 	<input type="text" bind:value={title} />
+
+	<p />
+
+	<a href="/guides">View Guides</a>
+	<a href="/about">About</a>
 </div>
 
 <style>
