491e1862718442be99011cd421ea0755af71af60
LWY <668915+laxa88@users.noreply.github.com>
2022-06-18 12:46:31 +0900

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script>
+	let title = 'Ninja Gaming Guides';
+
+	const setTitle = () => {
+		title = 'something completely different';
+	};
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>Lorem ipsum dolor sit amet</p>
+
+	<button on:click={setTitle}>Change tile</button>
+	<input type="text" bind:value={title} />
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
