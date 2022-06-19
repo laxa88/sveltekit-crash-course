f3c04238431d8e500c4bff4c74a2328171ceb47b
LWY <668915+laxa88@users.noreply.github.com>
2022-06-19 19:27:46 +0900

    add module to prefetch data

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,11 +1,43 @@
+<script context="module" lang="ts">
+	import type { LoadEvent } from '@sveltejs/kit';
+
+	type Guide = {
+		body: string;
+		id: number;
+		title: string;
+		userId: number;
+	};
+
+	export async function load({ fetch }: LoadEvent) {
+		const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const guides = await res.json();
+
+		if (res.ok) {
+			return {
+				props: {
+					hello: 'hello!',
+					guides
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error('Could not fetch guides')
+		};
+	}
+</script>
+
 <script lang="ts">
+	export let guides: Guide[];
 </script>
 
 <div class="guides">
 	<h2>Guides</h2>
 	<ul>
-		<li><a href="/">Guide 1</a></li>
-		<li><a href="/">Guide 2</a></li>
+		{#each guides as guide}
+			<li><a href="/">{guide.title}</a></li>
+		{/each}
 	</ul>
 </div>
 
