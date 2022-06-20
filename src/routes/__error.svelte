105eb49034e01bf093df3543802a9ce5fe2dfccb
LWY <668915+laxa88@users.noreply.github.com>
2022-06-20 22:29:18 +0900

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+	import type { LoadEvent, LoadOutput } from '@sveltejs/kit';
+
+	type PageProps = {
+		message?: string;
+	};
+
+	export function load({ error, status }: LoadEvent): LoadOutput<PageProps> {
+		return {
+			props: { message: error?.message }
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let message: string;
+</script>
+
+<div class="error">
+	<h2>Ooops</h2>
+	<p>{message}</p>
+	<a href="/guides">View all guides...</a>
+</div>
+
+<style>
+	.error {
+		text-align: center;
+		margin-bottom: 60px;
+	}
+</style>
